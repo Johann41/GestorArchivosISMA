dcd63e1740a7f3d7215af23588d7c799c156ceb9
unknown <en1gm4s30@gmail.com>
2022-09-08 03:04:26 -0500

    terminando home y agregando archivos html de los navs

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search_TJAM/', views.ventana_TJAM, name='TJAM'),
+    path('search_STEJEM/', views.ventana_STJEM, name='STEJEM'),
+    path('search_AGRARIOS/', views.ventana_AGRARIO, name='AGRARIOS'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html',{})
+
+def ventana_TJAM(request):
+    return render(request, 'home/TJAM.html',{})
+
+def ventana_STJEM(request):
+    return render(request, 'home/STEJEM.html',{})
+
+def ventana_AGRARIO(request):
+    return render(request, 'home/AGRARIO.html',{})
+
