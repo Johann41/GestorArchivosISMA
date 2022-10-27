0a4be00ed6e9c53030693502ef0fd311ea8138f6
unknown <en1gm4s30@gmail.com>
2022-10-27 08:25:49 -0500

    home v2

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login, name='auth_login'),
+    path('', views.login, name='auth_login'),
     path('signup/', views.signup, name='auth_signup'),
     path('password/', views.password, name='auth_password'),
 ]
